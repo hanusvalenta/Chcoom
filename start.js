469792ba1a2343d28faa9d9ad613878be520c628
Valenta Hanus <hanus.valenta@gmail.com>
2024-10-21 10:24:57 +0200

    do few things

--- start.js
+++ start.js
@@ -1,12 +1,22 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, Menu } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 1024,
+    height: 768,
+    minWidth: 800,
+    minHeight: 600,
+    icon: path.join(__dirname, 'icon.png'),
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+    }
   })
 
   win.loadFile('index.html')
+
+  Menu.setApplicationMenu(null);
 }
 
 app.whenReady().then(() => {
